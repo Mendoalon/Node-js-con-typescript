5e171e7d72c1442417a6c2616d3b09d3cdb7c555
Luis Mendoza <hyh@hotmail.com>
2023-11-30 18:12:47 -0500

    Correos electrónicos

--- 05-NOC/src/domain/entities/log.entity.ts
+++ 05-NOC/src/domain/entities/log.entity.ts
@@ -1,31 +1,45 @@
 export enum LogSeverityLevel {
-    low    = 'low',
-    medium = 'medium',
-    high   = 'high',
+  low = 'low',
+  medium = 'medium',
+  high = 'high',
+}
+
+export interface LogEntityOptions {
+  level: LogSeverityLevel;
+  message: string;
+  origin: string;
+  createdAt?: Date;
+}
+
+
+export class LogEntity {
+
+  public level: LogSeverityLevel; // Enum
+  public message: string;
+  public createdAt: Date;
+  public origin: string;
+
+  constructor( options: LogEntityOptions ) {
+    
+    const { message, level, origin, createdAt = new Date() } = options;
+    this.message = message;
+    this.level = level;
+    this.createdAt = createdAt;
+    this.origin = origin;
   }
-  
-  
-  
-  export class LogEntity {
-  
-    public level: LogSeverityLevel; // Enum
-    public message: string;
-    public createdAt: Date;
-  
-    constructor( message: string, level: LogSeverityLevel ) {
-      this.message = message;
-      this.level = level;
-      this.createdAt = new Date();
-    }
-  
-    //"{ "level": "high", "message":"Hola Mundo", "createdAt":"128937TZ12378123" }"
-    static fromJson = ( json: string ): LogEntity => {
-      const { message, level, createdAt } = JSON.parse(json);
-      
-      const log = new LogEntity(message, level);
-      log.createdAt = new Date(createdAt);
-      
-      return log;
-    }
-  
-  }
+
+  //"{ "level": "high", "message":"Hola Mundo", "createdAt":"128937TZ12378123" }"
+  static fromJson = ( json: string ): LogEntity => {
+    const { message, level, createdAt, origin } = JSON.parse( json );
+
+    const log = new LogEntity({ 
+      message,
+      level,
+      createdAt,
+      origin,
+    });
+
+    return log;
+  };
+
+}
