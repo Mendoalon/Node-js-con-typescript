51ab1916b1fa27c871683e7288adb80c90f5aa73
Luis Mendoza <hyh@hotmail.com>
2023-12-13 11:37:22 -0500

    REST API: Todo

--- 07-restweb/src/presentation/server.ts
+++ 07-restweb/src/presentation/server.ts
@@ -1,33 +1,42 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
     port: number;
-    public_path: string;
+    routes: Router;
+    public_path?: string;
+
 }
 
 export class Server {
     private app = express();
     private readonly port: number;
+    private readonly routes: Router;
     private readonly publicPath: string;
 
     constructor(options: Options) {
-        const { port, public_path = 'public' } = options;
+        const { port, routes, public_path = 'public' } = options;
         this.port = port;
         this.publicPath = public_path;
+        this.routes = routes;
     }
 
     async start() {
 
         //* Middleware
+        this.app.use( express.json());
 
-        //* Pucbli Folder
+        //* Public Folder
         this.app.use(express.static(this.publicPath));
 
+        //* Router
+        this.app.use( this.routes );
+
+
+        //* SPA
         this.app.get('*', (req, res) => {
             const indexPaht = path.join(__dirname, `../../${this.publicPath}/index.html`)
             res.sendFile(indexPaht);
-            return;
         });
 
         this.app.listen(this.port, () => {
